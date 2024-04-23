2e0059df49717f2266e97c28883b23dc6f7a144d
Rishabh-Raj-Prajapati <124397075+Rishabhraj01@users.noreply.github.com>
2024-04-23 08:57:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+
+const apiData = require("./data.json");
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello i am live");
+});
+
+app.get("./service", (req, res) => {
+  res.send(apiData);
+});
+
+app.listen(port, () => {
+  console.log("I am live again");
+});
